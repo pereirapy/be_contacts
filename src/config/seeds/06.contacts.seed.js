1958525210608097173fe4fe2d3ba566f8676064
Rodrigo Pereira <pereirapy@hotmail.com>
2022-03-04 13:02:41 -0300

    remove unused seeds

--- src/config/seeds/06.contacts.seed.js
+++
@@ -1,7 +0,0 @@
-const data = require('./contacts.json')
-const table = 'contacts'
-
-exports.seed = async function (knex) {
-  await knex(table).del()
-  return knex(table).insert(data)
-}
--- src/config/seeds/07.detailsContacts.seed.js
+++
@@ -1,7 +0,0 @@
-const data = require('./detailsContacts.json')
-const table = 'detailsContacts'
-
-exports.seed = async function (knex) {
-  await knex(table).del()
-  return knex(table).insert(data)
-}
